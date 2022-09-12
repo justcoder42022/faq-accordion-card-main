a93e1bcd1e36c0041f6bccbf30a9513c2766d460
justcoder42022 <gurpreet5kdev@gmail.com>
2022-09-12 20:52:53 +0530

    2nd commit

--- javaScript.js
+++ javaScript.js
@@ -1,38 +1,16 @@
+const detailsList = document.querySelectorAll("details");
+const summaryList = document.querySelectorAll("summary");
 
-const faqList = document.querySelectorAll(".faqItem");
-
-for(const faqItem of faqList){
-    faqItem.addEventListener("click", () => {
-   let question = faqItem.querySelector(".question");
-   let arrow =   faqItem.querySelector(".arrow");
-   let faqAns =  faqItem.querySelector(".faqAns");
-
-   if(question.classList.contains("questionSelected"))
-   {
-   question.classList.remove("questionSelected");
-   arrow.classList.remove("arrowSelected");
-   faqAns.classList.remove("faqAnsVisible");}
-   else{
-    question.classList.add("questionSelected");
-    arrow.classList.add("arrowSelected");
-    faqAns.classList.add("faqAnsVisible");
-   }
-      
-    })
+for (const summary of summaryList) {
+  summary.addEventListener("click", () => {
+    detailsClose();
+  });
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function detailsClose() {
+  for (const details of detailsList) {
+    if (details.hasAttribute("open")) {
+      details.removeAttribute("open");
+    }
+  }
+}
